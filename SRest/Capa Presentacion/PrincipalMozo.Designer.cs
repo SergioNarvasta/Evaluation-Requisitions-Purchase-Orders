fa87ceb86509e5d4bd521c8240aeea8dbf3e6a75
Sergio Narvasta <sergio.jn024@gmail.com>
2022-08-03 23:41:05 -0500

    Principal

--- SRest/Capa Presentacion/PrincipalMozo.Designer.cs
+++ SRest/Capa Presentacion/PrincipalMozo.Designer.cs
@@ -3,14 +3,9 @@ namespace SistemaRest.Capa_Presentacion
 {
     partial class PrincipalMozo
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+             private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
+        
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
