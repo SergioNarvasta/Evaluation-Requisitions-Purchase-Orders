d59be4de10e9a85b40b90f93c98c828265013ed4
Sergio Narvasta <sergio.jn024@gmail.com>
2023-01-11 10:27:41 -0500

    Dev UNegocioService

---
+++ Models/Helps/UNegocio.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+
+namespace HDProjectWeb.Models.Helps
+{
+    //Lista para mostrar los UNegocio en Combo (View Crear)
+    public class UNegocio
+    {
+        public string Codigo { get; set; }
+        public string Descri { get; set; }
+    }
+
+    public interface IUNegocioService
+    {
+        Task<IEnumerable<UNegocio>> ListaUNegocio();
+    }
+    public class UNegocioService : IUNegocioService
+    {
+        private readonly string connectionString;      
+
+        public UNegocioService(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");         
+        }
+        public async Task<IEnumerable<UNegocio>> ListaUNegocio()
+        {          
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<UNegocio>(@"SELECT ung_codepk as codigo,ung_deslar as descri 
+                     FROM UNID_NEGOCIO_UNG WHERE cia_codcia=1 and ung_estado=1");
+        }
+    }
+}
---
+++ Models/Helps/UNegocioViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HDProjectWeb.Models.Helps
+{
+    [ViewComponent(Name = "ListaUNegocio")]
+
+    //Lista para mostrar los TipoReq en Combo (View Crear)
+    public class UNegocioViewComponent : ViewComponent
+    {
+        private readonly IUNegocioService _unegocioService;
+        public UNegocioViewComponent(IUNegocioService unegocioService)
+        {
+           _unegocioService = unegocioService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var data = await _unegocioService.ListaUNegocio();
+            return View(data);
+        }
+    }
+}
